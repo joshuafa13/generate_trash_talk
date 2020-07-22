9adc0c63aec4c3275eda30ff8aaf7e1367ef4142
Josh Fang <joshuafang13@gmail.com>
2020-07-22 12:33:06 +0800

    generate trash talk init

---
+++ app.js
@@ -0,0 +1,32 @@
+// include modules & set variables
+const express = require('express');
+const app = express();
+const exphbs = require('express-handlebars');
+const port = 3000;
+const bodyParser = require('body-parser');
+const generateTrashTalk = require('./generateTrashTalk');
+
+// set template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// set body parser
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// set router
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.post('/', (req, res) => {
+  const options = req.body;
+
+  const trashTalk = generateTrashTalk(options);
+  console.log(options);
+  res.render('index', { trashTalk, options });
+});
+
+// listen
+app.listen(port, () => {
+  console.log(`Express listening to http://localhost:${port}`);
+});
